ee2fc2803c8f3148b7bd6de13f27cd3499db3fe3
Alex McLean <alex.mclean@bennetts.co.uk>
2022-05-24 15:58:20 +0100

    Creates data for the app

---
+++ src/data/policyData.js
@@ -0,0 +1,15 @@
+export const policyData = {
+    policyNumber: 'ABC123',
+    name: 'Alex McLean',
+    address: ['1 Road name', 'Town name', 'XX12 3XY'],
+    policyType: 'fullyComp',
+    price: {
+        total: '134.55',
+        type: 'monthly',
+    },
+    registration: 'KN58VFC',
+    paymentInformation: {
+        accountNumber: 123456789,
+        sortCode: '12-34-56',
+    }
+}
