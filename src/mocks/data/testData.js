9ae9892fe9f9192ff57fafaf272f50e65be6c246
Alex McLean <alex.mclean@bennetts.co.uk>
2022-05-31 11:45:39 +0100

    moves mock data into separate folder

---
+++ src/mocks/data/testData.js
@@ -0,0 +1,31 @@
+const testData = {
+    policyNumber: 'XYZ789',
+    name: 'Dave Smith',
+    address: ['22 Name ', 'City name', 'YY12 3ZY'],
+    policyType: 'thirdParty',
+    price: {
+        total: '400.55',
+        type: 'annual',
+    },
+    car: {
+        registration: 'FW32ETR',
+        make: 'BMW',
+        model: '132D',
+        value: 3000,
+        annualMilage: 20000,
+        purchaseDate: '06/01/2002',
+        overnightLocation: 'Garage',
+        registeredOwner: 'Policyholder',
+    },
+    policyDetails: {
+        start: '2022-04-03',
+        end: '2023-04-02',
+        NCB: '1',
+    },
+    paymentInformation: {
+        accountNumber: 987654321,
+        sortCode: '98-76-54',
+    }
+};
+
+export default testData;
